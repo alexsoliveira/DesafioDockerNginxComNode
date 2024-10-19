08eb8422d80c3fd8a7ed87e4cba5bc16405de4e0
alexsoliveira <alexsoliveira@gmail.com>
2024-10-19 12:51:02 -0300

    desafio docker Nginx com Node.js e Mysql

---
+++ node/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const config = {
+    host: 'database',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.stack);
+    return;
+  }
+  console.log('Conectado ao banco de dados MySQL.');
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM people';
+    
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('Erro ao consultar os registros:', err);
+            return res.status(500).send('Erro ao consultar os registros.');
+        }
+
+        let html = '<h1>Full Cycle Rocks!</h1>\n';
+        
+        results.forEach(user => {  
+            html += '<hr>';      
+            html += `<h2>Nome: ${user.name}</h2>`;
+            html += '<hr>';
+        });
+
+        res.send(html);
+    });
+});
+
+app.post('/pessoas', (req, res) => {
+    const { name } = req.body;
+    const sql = `INSERT INTO people(name) values( ? );`;
+  
+    connection.query(sql, [name], (err, results) => {
+        if (err) {
+            console.error('Erro ao inserir o registro:', err);
+            return res.status(500).send('Erro ao inserir o registro.');
+        }
+        res.status(201).send('Registro inserido com sucesso.');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Rodando na porta ${port}`);
+});
